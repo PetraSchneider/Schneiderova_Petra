6950fd39a734ffe06a6f7621a107c1483ab65a45
Petra Schneiderova <pe.schneiderova@gmail.com>
2022-12-18 21:43:27 +0100

    postup

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function konec (){
+    document.write("<footer>");
+    document.write("Schneiderová Petra, 2.IF");
+    document.write("</footer>");
+    document.write("</body>");
+    document.write("</html>");
+}
